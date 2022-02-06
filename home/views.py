d4e108bcec8d83a85b2acaa01f362fd42cd2b1d3
divyanshc33 <79359762+divyanshc33@users.noreply.github.com>
2022-02-06 13:50:55 +0530

    Initial commit

---
+++ home/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    # return HttpResponse("this is home page")
+    
+def about(request):
+    return HttpResponse("this is about page")
+    
+def contact(request):
+    return HttpResponse("this is contact page")
