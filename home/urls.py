d1b568f770703ced29673b65dfd1c6c27529c877
divyanshc33 <divu2000chouhan@gmail.com>
2022-02-27 19:35:48 +0530

    contact models

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from home import views
 
+#Adimn
+admin.site.site_header = "WarehouseX Admin"
+admin.site.site_title = "WarehouseX Admin Portal"
+admin.site.index_title = "Welcome to WarehouseX Portal"
+
 urlpatterns = [
-    path("",  views.index, name='home'),
-    path("location",  views.location, name='location'),
-    path("contact",  views.contact, name='contact'),
-    path("about",  views.about, name='about'),
-    path("contact",  views.contact, name='contact')
+    path("",  views.index, name = "home"),
+    path("location",  views.location, name = "location"),
+    path("contact",  views.contact, name = "contact"),
+    path("about",  views.about, name = "about"),
+    path("contact",  views.contact, name = "contact")
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -13,5 +15,12 @@ def about(request):
     # return HttpResponse("this is about page")
     
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date= datetime.today())
+        contact.save()
     return render(request, 'contact.html')
     # return HttpResponse("this is contact page")
